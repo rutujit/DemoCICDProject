dc194e7776fcf321df7ec5d5213f468c2c67d827
Rutujit Diwate <rutujit.diwate@saviant.local>
2021-08-18 01:41:29 +0530

    third commit

--- src/test/java/testpackage/FirstClass.java
+++ src/test/java/testpackage/FirstClass.java
@@ -1,8 +1,14 @@
 package testpackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FirstClass {
+	
+	public WebDriver driver;
 	
 	@Test
 	public void testFirst()
@@ -13,5 +19,13 @@ public class FirstClass {
 	@Test
 	public void testSecond() {
 		System.out.println("Second test");
+	}
+	
+	@Test
+	public void launchBrowser()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://www.google.com");
 	}
 }
