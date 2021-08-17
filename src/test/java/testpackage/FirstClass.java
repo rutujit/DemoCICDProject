67ba0e1f44d00991e105e5ec316793549329cd50
Rutujit Diwate <rutujit.diwate@saviant.local>
2021-08-18 02:38:01 +0530

    Running tests from POM through testng

--- src/test/java/testpackage/FirstClass.java
+++ src/test/java/testpackage/FirstClass.java
@@ -1,5 +1,6 @@
 package testpackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -11,21 +12,31 @@ public class FirstClass {
 	public WebDriver driver;
 	
 	@Test
-	public void testFirst()
+	public void firstTest()
 	{
 		System.out.println("Test Case 1");
 	}
 
 	@Test
-	public void testSecond() {
+	public void secondTest() {
 		System.out.println("Second test");
 	}
 	
 	@Test
-	public void launchBrowser()
+	public void launchBrowserTest()
 	{
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 	}
+	
+	@Test
+	public void failedTest()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://pmmssaviantvm.centralindia.cloudapp.azure.com/account/login");
+		driver.findElement(By.name("password2"));
+	}
 }
+
